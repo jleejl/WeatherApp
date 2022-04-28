5b606d51513a5ea31db663e897cd499ddfe6f799
jleejl <jacklee0284@gmail.com>
2022-04-28 18:47:54 -0500

    Assignment 8

--- app/src/main/java/com/ics342/weatherapp/CurrentConditionsFragment.kt
+++ app/src/main/java/com/ics342/weatherapp/CurrentConditionsFragment.kt
@@ -1,10 +1,7 @@
 package com.ics342.weatherapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
--- app/src/main/java/com/ics342/weatherapp/ForecastFragment.kt
+++ app/src/main/java/com/ics342/weatherapp/ForecastFragment.kt
@@ -1,9 +1,7 @@
 package com.ics342.weatherapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
